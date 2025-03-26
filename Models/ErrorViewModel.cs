0c7b9035d00c282ba88706bd5a6e2ee8a65c5cb3
NurulAinFarinaRaman <97277671+eynaraa@users.noreply.github.com>
2025-03-26 12:11:16 +0800

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Toothcare_Appointment_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
